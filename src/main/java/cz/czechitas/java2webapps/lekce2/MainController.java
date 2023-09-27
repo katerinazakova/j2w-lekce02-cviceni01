be8c13705762c5d6a2e2efdb9a86a7759e453144
7ZKaty <zakova.kz@gmail.com>
2023-09-27 14:28:24 +0200

    Vytvoření metod datum a čas

---
+++ src/main/java/cz/czechitas/java2webapps/lekce2/MainController.java
@@ -0,0 +1,31 @@
+package cz.czechitas.java2webapps.lekce2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+@Controller
+public class MainController {
+
+    private static final Locale LOCALE = Locale.forLanguageTag("cs-CZ");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("d. MMMM yyyy", LOCALE);
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("H:mm", LOCALE);
+    @GetMapping("/date")
+    public ModelAndView makeDate() {
+        ModelAndView result = new ModelAndView("date");
+        String formattedDate = LocalDate.now().format(DATE_FORMATTER);
+        result.addObject("date", formattedDate);
+        return result;
+    }
+
+    @GetMapping("/time")
+    public ModelAndView makeTime() {
+        ModelAndView result = new ModelAndView("time");
+        String formattedTime = LocalTime.now().format(TIME_FORMATTER);
+        result.addObject("time", formattedTime);
+        return result;
+    }
+}
